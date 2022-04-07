232828dd279979ae860ade2c87d5a397d802d47c
Toriola Faidat <faaedah@gmail.com>
2022-04-07 12:23:44 +0100

    added 'aria expanded' to navbar

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const menu = document.querySelector('#hamburger');
+const menuBtn = document.querySelector('#hamburger');
 const links = document.querySelector('nav div');
-const ul = document.querySelector('ul');
 const closeIcon = document.querySelector('#close-icon');
+let navLinks = document.querySelectorAll('nav li');
 
-//Toggle to menu hamburger
-menu.addEventListener('click', function(){
-links.classList.toggle('show-links');
-ul.classList.toggle('show-lists')
-});
+//TOGGLE HAMBURGER MENU BTN
+menuBtn.addEventListener('click', () => {
+    if (links.classList.contains('show-links')) {
+        links.classList.remove('show-links');
+        menuBtn.setAttribute("aria-expanded", false)
+    } else {
+        links.classList.add('show-links');
+        menuBtn.setAttribute("aria-expanded", true)
+    }
+})
 
 //Close the side bar once the close icon gets clicked
 //closeIcon.addEventListener('click', function(){
 //    links.classList.remove('show-links');
+      // menuBtn.setAttribute("aria-expanded", false)
 //});
 
-let navLinks = document.querySelectorAll('.nav-link');
-//event listener for each links
+//CLOSE THE NAV-BAR ONCE A LINK IS CLICKED
 navLinks.forEach(link => {  
   link.addEventListener('click', () => {
     links.classList.remove('show-links');
+    menuBtn.setAttribute("aria-expanded", false)
   }); 
 });
 
-//Update the copyright year automatically
+//UPDATE THE COPYRIGHT YEAR AUTOMATICALLY
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
