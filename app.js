927250d5734713299e33aa5fa8b958bb7c8fa7bc
Toriola Faidat <faaedah@gmail.com>
2022-04-07 17:15:01 +0100

    added images for delivery section

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector('#hamburger');
 const links = document.querySelector('nav div');
 const closeIcon = document.querySelector('#close-icon');
-let navLinks = document.querySelectorAll('nav li');
+let navLists = document.querySelectorAll('nav li');
 
 //TOGGLE HAMBURGER MENU BTN
 menuBtn.addEventListener('click', () => {
@@ -21,12 +21,20 @@ menuBtn.addEventListener('click', () => {
 //});
 
 //CLOSE THE NAV-BAR ONCE A LINK IS CLICKED
-navLinks.forEach(link => {  
+navLists.forEach(link => {  
   link.addEventListener('click', () => {
     links.classList.remove('show-links');
     menuBtn.setAttribute("aria-expanded", false)
   }); 
 });
+
+//const activePage = window.location.pathname;
+//const navLinks = document.querySelectorAll('nav li a').forEach(link => {
+ // if(link.href.includes(`${activePage}`)){
+ //   link.setAttribute("aria-current", 'page')
+ //   console.log(link);
+ // }
+//})
 
 //UPDATE THE COPYRIGHT YEAR AUTOMATICALLY
 const date = document.getElementById("date");
